24a3c7fe35957b36234306b64198eafb77112ce7
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-03-09 23:44:53 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -9,19 +9,28 @@ int main(void)
     {
         n = get_int("How tall is the pyramid?  ");
     }
-    while ( n > 8 || n < 1);
+    while (n > 8 || n < 1);
 
 // row
-    for ( int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         // column
-            for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
+        {
+            if (i + j < n - 1)
             {
-                if ( i + j < n - 1)
-                    printf(" ");
-                else
+
+                printf(" ");
+            }
+
+            else
+            {
+
+
                 printf("#");
             }
+        }
+
         // next row
         printf("\n");
     }
