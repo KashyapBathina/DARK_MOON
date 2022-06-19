02db595efe6fb39d77ae5b9490445823b9904e1d
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-06-19 00:28:59 +0000

    automated commit by submit50

--- inheritance.c
+++ inheritance.c
@@ -54,7 +54,7 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = p->parents[0]->alleles[rand() %2 ];
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2 ];
         p->alleles[1] = p->parents[1]->alleles[rand() % 2];
     }
 
@@ -77,7 +77,8 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p == NULL){
+    if (p == NULL)
+    {
         return;
     }
 
