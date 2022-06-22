1fc634db0d103ec95edb5370ff3798b874e4d474
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-06-22 23:03:41 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,10 +1,10 @@
 # TODO
 from cs50 import get_int
 
-
-height = get_int("Height: ")
-if height <= 1 or height >= 8:
-    exit()
+while True:
+    height = get_int("Height: ")
+    if height <= 1 or height >= 8:
+        break
 
 for i in range(height):
     print((height - 1 - i) * " ", end = "")
