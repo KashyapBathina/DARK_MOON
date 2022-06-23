c984d043667167a3388074eb506203dd517b08e3
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-06-23 00:11:27 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -2,7 +2,7 @@
 from cs50 import get_string
 
 letters = 0
-words = 0
+words = 1
 sentences = 0
 
 text = get_string("Text: ")
@@ -17,11 +17,12 @@ for i in range(len(text)):
     if (text[i] == "." or text[i] == "!" or text[i] == "?"):
         sentences += 1
 
-level = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+calculation = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+level = round(calculation)
 
-if level > 1:
+if level < 1:
     print("Below Grade 1")
-elif level < 12:
+elif level > 16:
     print("Grade 16+")
 else:
-    print(f"Grade {round(level)}")
+    print(f"Grade {level}")
