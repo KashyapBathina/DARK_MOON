38c60030ed242ea961960c56a0f06753f96f1144
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-06-23 22:09:38 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -22,7 +22,7 @@ def main():
 
     # This results in "name" : 0
     for i in database[0]:
-    # where sequence is the full dna sequence and i is the subsequence like agat
+        # where sequence is the full dna sequence and i is the subsequence like agat
         matches[i] = (longest_match(sequence, i))
 
     # TODO: Check database for matching profiles
