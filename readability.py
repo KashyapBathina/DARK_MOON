077b588e759641d5eaac77d50f774960e4d0809a
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-06-23 00:03:14 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -14,7 +14,6 @@ for i in range(len(text))
     if (text[i]"." or text[i] = "!" or text[i] = "?"):
         sentences += 1
 
-grade level = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+level = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
-
-
+print(f"Grade {level})
