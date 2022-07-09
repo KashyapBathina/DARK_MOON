2d0913f2bf00762388be575499162bf3f6c64a23
KashyapBathina <KashyapBathina@users.noreply.github.com>
2022-07-09 23:08:03 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def buy():
         if (not request.form.get("symbol")) or (not request.form.get("shares")):
             return apology("must provide stock symbol and number of shares", 403)
 
-        if int(request.form.get("shares")) <= 0 or not .is_integer(int(request.form.get("shares"))):
+        if int(request.form.get("shares")) <= 0:
             return apology("must provide valid number of shares", 400)
 
         quote = lookup(request.form.get("symbol"))
